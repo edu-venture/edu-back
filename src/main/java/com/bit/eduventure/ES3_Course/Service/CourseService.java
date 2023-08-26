f5e0c05e258b4fb6f2e154daa1f33c66efc8c031
minje <alswp0910@naver.com>
2023-08-26 13:12:30 +0900

    feat: course생성 기능 구현

--- src/main/java/com/bit/eduventure/ES3_Course/Service/CourseService.java
+++ src/main/java/com/bit/eduventure/ES3_Course/Service/CourseService.java
@@ -2,6 +2,7 @@ package com.bit.eduventure.ES3_Course.Service;
 
 
 import com.bit.eduventure.ES1_User.Entity.User;
+import com.bit.eduventure.ES3_Course.DTO.CourseDTO;
 import com.bit.eduventure.ES3_Course.Entity.Course;
 
 import java.util.List;
@@ -22,5 +23,5 @@ public interface CourseService {
 
     Course findByTeacherId(int id);
 
-    void createCourse(User user);
+    void createCourse(CourseDTO courseDTO);
 }
