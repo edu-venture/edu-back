9aa6ba3a21cbfba3233cd522af9bedc993bdbfc2
stats-dev <hohun94@naver.com>
2023-08-31 20:11:24 +0900

    feat: 매 자정마다 퇴실기록 없으면 결석(2)로 변경 스케줄링

--- src/main/java/com/bit/eduventure/EduVentureApplication.java
+++ src/main/java/com/bit/eduventure/EduVentureApplication.java
@@ -2,8 +2,10 @@ package com.bit.eduventure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EduVentureApplication {
 
     public static void main(String[] args) {
