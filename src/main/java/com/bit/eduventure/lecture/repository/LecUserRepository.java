230350c4112a54a20bf7d2f5fd3a779e701878e5
stats-dev <hohun94@naver.com>
2023-09-01 17:30:10 +0900

    fix: 불필요한 Lecture 파일 수정

--- src/main/java/com/bit/eduventure/lecture/repository/LecUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bit.eduventure.lecture.repository;
-
-import com.bit.eduventure.lecture.entity.LecUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LecUserRepository extends JpaRepository<LecUser, Integer> {
-}
